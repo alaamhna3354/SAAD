da14ccbb13de20fe6f1b16183165931b317818b8
alaamhna3354 <76902248+alaamhna3354@users.noreply.github.com>
2022-05-15 18:23:47 -0400

    edit login design

--- resources/views/templates/basic/user/auth/register.blade.php
+++ resources/views/templates/basic/user/auth/register.blade.php
@@ -6,22 +6,7 @@
             <img src="{{asset($activeTemplateTrue.'images/round.png')}}" alt="shape">
         </div>
         <div class="container">
-            <figure class="figure highlight-background highlight-background--lean-left">
-                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1439px"
-                     height="480px">
-                    <defs>
-                        <linearGradient id="PSgrad_1" x1="42.262%" x2="0%" y1="90.631%" y2="0%">
-                            <stop offset="28%" stop-color="rgb(245,246,252)" stop-opacity="1"/>
-                            <stop offset="100%" stop-color="rgb(255,255,255)" stop-opacity="1"/>
-                        </linearGradient>
-
-                    </defs>
-                    <path fill-rule="evenodd" fill="rgb(255, 255, 255)"
-                          d="M863.247,-271.203 L-345.788,-427.818 L760.770,642.200 L1969.805,798.815 L863.247,-271.203 Z"/>
-                    <path fill="url(#PSgrad_1)"
-                          d="M863.247,-271.203 L-345.788,-427.818 L760.770,642.200 L1969.805,798.815 L863.247,-271.203 Z"/>
-                </svg>
-            </figure>
+           
 
             <div class="account-wrapper">
                 <div class="login-area account-area change-form">
